b4a6ad50cdca555ae1c4b12e077624ef93c29d12
Akshay1-ARJ <92629214+Akshay1-ARJ@users.noreply.github.com>
2022-09-11 15:27:02 +0530

    AJ

---
+++ internetuseageRunner (1).java
@@ -0,0 +1,30 @@
+ class internetuseageRunner{
+	 public static void main (String[] args)
+	 {
+		 int youtube=24;
+         int snapchat=23;		 
+		 int whatsap=22;
+		 int hotstar=20;
+		 int netflex=19;
+		 
+		 int[] netuseage = {youtube,snapchat,whatsap,hotstar,netflex};
+	     int numberofapps=netuseage.length;
+		 System.out.println("Number of apps : "+numberofapps);
+		 int ref0 = netuseage[0];
+		 int ref1 = netuseage[1];
+		 int ref2 = netuseage[2];
+		 int ref3 = netuseage[3];
+		 int ref4 = netuseage[4];
+		 
+		 System.out.println("harish");
+		 System.out.println(ref1);
+		 System.out.println(ref2);
+		 System.out.println(ref3);
+		 System.out.println(ref4);
+		 
+	 }
+	 
+	 
+	 
+	 
+ }
---
+++ patient (1).java
@@ -0,0 +1,10 @@
+class Patient {
+      public static void main(String[] args){
+       	 String name = "Baba";
+		 int age = 40;
+		 System.out.println("age is : "+ age + "\nname is : " +name);
+   }
+	
+	
+
+}
